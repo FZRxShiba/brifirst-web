a8c3c340158e3579acf9c637600f3d96c52493b1
alfnms <alfnms@gmail.com>
2022-11-09 09:53:56 +0700

    card menu and benefit section

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,18 +1,12 @@
 import React from "react";
-import CustomNavbar from "../../components/CustomNavbar/CustomNavbar";
-import CustomFooter from "../../components/CustomFooter/CustomFooter"
-import Modul from "../../components/Modul/Modul"
-import HeaderCarousel from "../../components/HeaderCarousel/HeaderCarousel"
-import NewsCarousel from "../../components/NewsCarousel/NewsCarousel"
+import CardMenu from "../../components/CardMenu/CardMenu";
+import BenefitSection from "../../components/BenefitSection/BenefitSection";
 
 const Homepage = () => {
   return (
-    <div style={{"backgroundColor":"#18B1E0"}}>
-      <CustomNavbar />
-      <HeaderCarousel />
-      <Modul />
-      <NewsCarousel />
-      <CustomFooter />
+    <div>
+      <CardMenu />
+      <BenefitSection />
     </div>
   );
 };
