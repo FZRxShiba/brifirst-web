4d93d9cd6d5f1a2bed15bd990eb5bcf5211accd7
bagas-09 <93715655+bagas-09@users.noreply.github.com>
2022-11-09 11:00:03 +0700

    carousel board new

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -2,12 +2,14 @@ import React from "react";
 import CardMenu from "../../components/CardMenu/CardMenu";
 import BenefitSection from "../../components/BenefitSection/BenefitSection";
 import HeaderCarousel from "../../components/HeaderCarousel/HeaderCarousel"
+import BoardCarousel from "../../components/BoardCarousel/BoardCarousel";
 
 const Homepage = () => {
   return (
     <div>
       <HeaderCarousel />
       <CardMenu />
+      <BoardCarousel/>
       <BenefitSection />
     </div>
   );
