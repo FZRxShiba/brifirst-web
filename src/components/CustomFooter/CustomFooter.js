49153cfc57cb01470b284fceedb59db6b00e5202
alfnms <alfnms@gmail.com>
2022-11-09 10:01:20 +0700

    fix bug on footer

--- src/components/CustomFooter/CustomFooter.js
+++ src/components/CustomFooter/CustomFooter.js
@@ -1,8 +1,8 @@
 import React from "react";
 import LogoSAP from "./SAP.svg";
 import LogoDeloitte from "./deloitte.png";
-import { FiMapPin,FiMail } from "react-icons/fi";
-import { BsTelephoneFill, BsTelephone } from "react-icons/bs";
+import { FiMapPin, FiMail } from "react-icons/fi";
+import { BsTelephone } from "react-icons/bs";
 import { AiOutlineInstagram } from "react-icons/ai";
 import { MDBFooter, MDBContainer, MDBRow, MDBCol } from "mdb-react-ui-kit";
 import "./CustomFooter.css";
@@ -38,23 +38,24 @@ export default function App() {
             <MDBCol md="4" lg="3" xl="3" className="mx-auto mb-md-0 mb-4">
               <h6
                 className="text-uppercase fw-bold mb-4"
-                style={{ color: "#18B1E0"}}
+                style={{ color: "#18B1E0" }}
               >
                 Contact
               </h6>
-              <div className="dotremovecol"><Row>
-                <Col sm={1}>
-                  <BsTelephone />
-                </Col>
-                <Col sm={11}>
-                  <div className="marjan">
-                  <ul>
-                    <li>+62 21 5758534 </li>
-                    <li>+62 21 5752770 </li>
-                    <li>+62 21 5752733  </li>
-                  </ul></div>
-                </Col>
-              </Row></div>
+              <div className="dotremovecol">
+                <Row>
+                  <Col sm={2}>
+                    <BsTelephone />
+                  </Col>
+                  <Col sm={10}>
+                    <ul>
+                      <li>+62 21 5758534 </li>
+                      <li>+62 21 5752770 </li>
+                      <li>+62 21 5752733 </li>
+                    </ul>
+                  </Col>
+                </Row>
+              </div>
 
               <p>
                 <FiMail size={20} />
@@ -91,10 +92,12 @@ export default function App() {
         </MDBContainer>
       </section>
 
-      <div className="footer text-left p-2 footBold" >
-       <div className="footermargin">© 2022 Copyright PT Bank Rakyat Indonesia (Persero) Tbk. All Right
-        Reserved.
-        <a className="'text-reset fw-bold'" href=""></a></div> 
+      <div className="footer text-left p-2 footBold">
+        <div className="footermargin">
+          © 2022 Copyright PT Bank Rakyat Indonesia (Persero) Tbk. All Right
+          Reserved.
+          <a className="'text-reset fw-bold'" href=""></a>
+        </div>
       </div>
     </MDBFooter>
   );
