ff5693c43671780b4c502c73fb1a845ee8287b72
frostfrost2022 <fzr2k2021@gmail.com>
2022-11-10 23:15:13 +0700

    News Carousel Fix

--- src/components/NewsCarousel/NewsCarousel.js
+++ src/components/NewsCarousel/NewsCarousel.js
@@ -5,53 +5,42 @@ import news1 from "./news1.png";
 import Carousel from "react-bootstrap/Carousel";
 
 const NewsCarousel = () => {
-  
   return (
     <div
-      className="bgcardnews" id="menu-newsku"
+      className="bgcardnews"
+      id="menu-newsku"
       style={{
         background: "linear-gradient(90deg, #d9f4ff 10.98%, #bec9e7 100%)",
-        padding: "30px",
+        padding: "100px",
       }}
     >
       <Row>
-        <Col
-          xs={8}
-          style={{ fontSize: "35px", paddingTop: "100px", textAlign: "center" }}
-        >
-         <div className="textcolorku" style={{color:"blue"}}>Temukan update & <p><b>berita terbaru kami.</b></p>
+        <Col xs={8} style={{}}>
+          <div className="textcolorku" style={{ color: "blue", fontSize: "75px", paddingTop: "30px", paddingLeft: "50px", textAlign: "left" }}>
+            Temukan update &{" "}
+            <p>
+              <b>berita terbaru kami.</b>
+            </p>
+          </div>
+          <p></p>
           <br></br>
           <br></br>
-          <p><b style={{fontSize:"25px"}}>LIHAT LEBIH ></b></p></div> 
+          <br></br>
+          <br></br>
+          <p>
+            <b style={{ fontSize: "35px", color: "blue", paddingLeft: "300px" }}>LIHAT LEBIH ></b>
+          </p>
         </Col>
         <Col>
-          <Carousel autoPlay={true}
-    interval={5000}
-    controls={false}
-    indicators={false}>
+          <Carousel autoPlay={true} interval={5000} controls={false} indicators={false}>
             <Carousel.Item>
-              <img
-                className="d-block w-5"
-                src={news1}
-                alt="First slide"
-                style={{ height: "355px", width: "282px" }}
-              />
+              <img className="d-block w-5" src={news1} alt="First slide" style={{ height: "433px", width: "309px", boxShadow: "-1px 2px 9px #F4AAB9", padding: "-1em" }} />
             </Carousel.Item>
             <Carousel.Item>
-              <img
-                className="d-block w-5"
-                src={news1}
-                alt="Second slide"
-                style={{ height: "355px", width: "282px" }}
-              />
+              <img className="d-block w-5" src={news1} alt="Second slide" style={{ height: "433px", width: "309px", boxShadow: "-1px 2px 9px #F4AAB9", padding: "-1em" }} />
             </Carousel.Item>
             <Carousel.Item>
-              <img
-                className="d-block w-5"
-                src={news1}
-                alt="Third slide"
-                style={{ height: "355px", width: "282px" }}
-              />
+              <img className="d-block w-5" src={news1} alt="Third slide" style={{ height: "433px", width: "309px", boxShadow: "-1px 2px 9px #F4AAB9", padding: "-1em" }} />
             </Carousel.Item>
           </Carousel>
         </Col>
