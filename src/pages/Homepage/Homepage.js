a89107c9b5f208866e57f5b28dbb0075035327db
alfnms <alfnms@gmail.com>
2022-11-10 13:45:30 +0700

    add radius to div

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,15 +1,21 @@
 import React from "react";
 import CardMenu from "../../components/CardMenu/CardMenu";
 import BenefitSection from "../../components/BenefitSection/BenefitSection";
-import HeaderCarousel from "../../components/HeaderCarousel/HeaderCarousel"
+import HeaderCarousel from "../../components/HeaderCarousel/HeaderCarousel";
 import BoardCarousel from "../../components/BoardCarousel/BoardCarousel";
 
 const Homepage = () => {
   return (
-    <div style={{ background: "linear-gradient(90.1deg, #d0e4ff 10.98%, #ffffff 99.93%)"}}>
+    <div
+      style={{
+        background: "linear-gradient(90.1deg, #d0e4ff 10.98%, #ffffff 99.93%)",
+      }}
+    >
       <HeaderCarousel />
       <CardMenu />
-      <BoardCarousel/>
+      <BoardCarousel />
+      <br /> <br />
+      <br />
       <BenefitSection />
     </div>
   );
